d850e531ae809fa0928889583ded7e11584a087a
ovsky <przemek.orlowski@outlook.com>
2025-04-15 12:34:36 +0200

    Update Strings and Names

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -104,7 +104,7 @@ android {
                 signingConfigs.getByName("default")
             }
 
-            resValue("string", "app_name_suffixed", "Sudachi")
+            resValue("string", "app_name_suffixed", "Redachi")
             isMinifyEnabled = true
             isDebuggable = false
             proguardFiles(
@@ -117,7 +117,7 @@ android {
         // Attaches 'debug' suffix to version and package name, allowing installation alongside the release build.
         register("relWithDebInfo") {
             isDefault = true
-            resValue("string", "app_name_suffixed", "Sudachi Debug Release")
+            resValue("string", "app_name_suffixed", "Redachi Debug Release")
             signingConfig = signingConfigs.getByName("default")
             isMinifyEnabled = true
             isDebuggable = true
@@ -134,7 +134,7 @@ android {
         // Attaches 'debug' suffix to version and package name, allowing installation alongside the release build.
         debug {
             signingConfig = signingConfigs.getByName("default")
-            resValue("string", "app_name_suffixed", "Sudachi Debug")
+            resValue("string", "app_name_suffixed", "Redachi Debug")
             isDebuggable = true
             isJniDebuggable = true
             versionNameSuffix = "-debug"
